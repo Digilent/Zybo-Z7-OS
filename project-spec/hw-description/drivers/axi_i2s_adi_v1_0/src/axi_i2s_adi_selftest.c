65c822712d6bac775af88ddfaccce5b22468a75a
Demon000 <demonsingur@gmail.com>
2020-10-30 13:03:44 +0200

    Z7-20: Import hardware handoff from cd5458b

--- project-spec/hw-description/drivers/axi_i2s_adi_v1_0/src/axi_i2s_adi_selftest.c
+++ project-spec/hw-description/drivers/axi_i2s_adi_v1_0/src/axi_i2s_adi_selftest.c
@@ -1,6 +1,6 @@
 
 /***************************** Include Files *******************************/
-#include "PWM.h"
+#include "axi_i2s_adi.h"
 #include "xparameters.h"
 #include "stdio.h"
 #include "xil_io.h"
@@ -17,7 +17,7 @@
  * If the hardware system is not built correctly, this function may never
  * return to the caller.
  *
- * @param   baseaddr_p is the base address of the PWMinstance to be worked on.
+ * @param   baseaddr_p is the base address of the AXI_I2S_ADIinstance to be worked on.
  *
  * @return
  *
@@ -28,7 +28,7 @@
  * @note    Self test may fail if data memory and device are not on the same bus.
  *
  */
-XStatus PWM_Reg_SelfTest(void * baseaddr_p)
+XStatus AXI_I2S_ADI_Reg_SelfTest(void * baseaddr_p)
 {
 	u32 baseaddr;
 	int write_loop_index;
@@ -47,9 +47,9 @@ XStatus PWM_Reg_SelfTest(void * baseaddr_p)
 	xil_printf("User logic slave module test...\n\r");
 
 	for (write_loop_index = 0 ; write_loop_index < 4; write_loop_index++)
-	  PWM_mWriteReg (baseaddr, write_loop_index*4, (write_loop_index+1)*READ_WRITE_MUL_FACTOR);
+	  AXI_I2S_ADI_mWriteReg (baseaddr, write_loop_index*4, (write_loop_index+1)*READ_WRITE_MUL_FACTOR);
 	for (read_loop_index = 0 ; read_loop_index < 4; read_loop_index++)
-	  if ( PWM_mReadReg (baseaddr, read_loop_index*4) != (read_loop_index+1)*READ_WRITE_MUL_FACTOR){
+	  if ( AXI_I2S_ADI_mReadReg (baseaddr, read_loop_index*4) != (read_loop_index+1)*READ_WRITE_MUL_FACTOR){
 	    xil_printf ("Error reading register value at address %x\n", (int)baseaddr + read_loop_index*4);
 	    return XST_FAILURE;
 	  }
